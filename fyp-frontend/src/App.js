71251f66df86baef3ee22dfb2ef5526e0f2169dc
moodmelody2 <mood.melody2@gmail.com>
2025-12-28 14:33:07 +0500

    Update App.js

--- fyp-frontend/src/App.js
+++ fyp-frontend/src/App.js
@@ -5,7 +5,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Footer from "./components/Footer";
 
-import { BrowserRouter as Router, Routes, Route, NavLink, Link } from 'react-router-dom';
+import { Routes, Route, NavLink, Link } from 'react-router-dom';
 
 import Home from './components/Home';
 import Upload from './components/Uploads';
@@ -15,56 +15,56 @@ import Result from './components/Result';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header>
-          <Navbar expand="lg" className="navbackground">
-            <Container>
-              <Navbar.Brand as={Link} to="/" class="logo">
-                <img
-                  src={`${process.env.PUBLIC_URL}/fyp-images/moodmelodyicon.png`}
-                  width="60"
-                  height="50"
-                  alt="MoodMelody Logo"
-                /><h7>MoodMelody</h7>
-              </Navbar.Brand>
+    <div className="App">
+      <header>
+        <Navbar expand="lg" className="navbackground" variant="dark">
+          <Container>
+            <Navbar.Brand as={Link} to="/" className="logo">
+              <img
+                src={`${process.env.PUBLIC_URL}/fyp-images/moodmelodyicon.png`}
+                width="50"
+                height="50"
+                alt="MoodMelody Logo"
+              />
+              <span className="brand-text">MoodMelody</span>
+            </Navbar.Brand>
 
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="nav-links mx-auto">
-                  <NavLink to="/" className="nav-link">Home</NavLink>
-                  <NavLink to="/upload" className="nav-link">Upload</NavLink>
-                  <NavLink to="/keyword" className="nav-link">Keyword</NavLink>
-                  <NavLink to="/processing" className="nav-link">Processing</NavLink>
-                  <NavLink to="/result" className="nav-link">Result</NavLink>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-        </header>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+              <Nav className="nav-links">
+                <NavLink to="/" className="nav-link">Home</NavLink>
+                <NavLink to="/upload" className="nav-link">Upload</NavLink>
+                <NavLink to="/keyword" className="nav-link">Keyword</NavLink>
+                <NavLink to="/processing" className="nav-link">Processing</NavLink>
+                <NavLink to="/result" className="nav-link">Result</NavLink>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
 
-       <main
-  className="App-main"
-  style={{
-    backgroundImage: `url(${process.env.PUBLIC_URL}/fyp-images/background.jpeg)`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    minHeight: "100%",
-    color: "white"
-  }}
->
-  <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/upload" element={<Upload />} />
-            <Route path="/keyword" element={<Keyword />} />
-            <Route path="/processing" element={<Processing />} />
-            <Route path="/result" element={<Result />} />
-          </Routes>
-        </main>
-        <Footer /> 
-      </div>
-    </Router>
+      <main
+        className="App-main"
+        style={{
+          backgroundImage: `url(${process.env.PUBLIC_URL}/fyp-images/background.jpeg)`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          minHeight: "100vh",
+          color: "white"
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/keyword" element={<Keyword />} />
+          <Route path="/processing" element={<Processing />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
   );
 }
 
